75d3c0f8f9750e813d9ee973346ee118a72db6bb
Per Ploug <per.ploug@zalando.de>
2018-11-01 16:27:31 +0100

    fix merge mistake
    Signed-off-by: Per Ploug <per.ploug@zalando.de>

--- run.js
+++ run.js
@@ -57,10 +57,14 @@ const timePassed = function(startTime) {
 
 async function init() {
   var token = process.argv[2];
+  var orgsFilter = process.argv[3] || '*';
+  var tasksFilter = process.argv[4] || '*';
 
+  if (tasksFilter !== '*') {
     tasksFilter = tasksFilter.split(',');
   }
 
+  if (orgsFilter !== '*') {
     orgsFilter = orgsFilter.split(',');
   }
 
@@ -80,8 +84,6 @@ async function init() {
   // Iterate through all orgs and collect members and repos
   for (let org of orgs) {
     if (runTask(org, orgsFilter)) {
-=======
->>>>>>> bc6446e95123c7d6a6ddf556aff3de1bfb94ee03
       console.log(` ⬇️  Downloading ${org.login}`);
 
       // Get the org details and save it
